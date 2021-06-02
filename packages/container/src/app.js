a24f4b2cb6212eea5035275718eabd8783bef52d
Humberto <humberto.g.moreira@gmail.com>
2021-06-02 11:13:18 -0300

    Added dashbaord

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,28 +1,45 @@
-import React, {lazy, Suspense, useState} from 'react'
+import React, {lazy, Suspense, useState, useEffect} from 'react'
 import Header from './components/Header'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Router, Redirect, useHistory } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 import {StylesProvider, createGenerateClassName} from "@material-ui/core/styles"
 import ProgressBar from "./components/Progress"
 
 const MarketingAppLazy = lazy(()=>import ('./components/MarketingApp'))
 const AuthAppLazy = lazy(()=>import ('./components/AuthApp'))
+const DashboardAppLazy = lazy(()=>import ('./components/DashboardApp'))
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
 })
 
+const history = createBrowserHistory()
+
 export default ()=>{
 
     const [isSignedIn, setIsSignedIn] = useState(false)
+
+    useEffect(()=>
+        {
+            if(isSignedIn){
+                history.push('/dashboard')
+            }
+    }, [isSignedIn])
+
+
     return   ( 
-        <BrowserRouter>
+        <Router history={history}>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header isSignedIn={isSignedIn} onSignOut={()=> setIsSignedIn(false)}/>
                     <Suspense fallback={<ProgressBar/>}>
                         <Switch>
                             <Route path="/auth">
-                            <AuthAppLazy onSignIn={()=> setIsSignedIn(true)}/>
-                                </Route>
+                                <AuthAppLazy onSignIn={()=> setIsSignedIn(true)}/>
+                            </Route>
+                            <Route path="/dashboard">
+                                {!isSignedIn && <Redirect to="/"/>}
+                                <DashboardAppLazy />
+                            </Route>
                             <Route path="/"  >
                                 <MarketingAppLazy />
                             </Route>
@@ -30,6 +47,6 @@ export default ()=>{
                     </Suspense>
                 </div>
             </StylesProvider>
-        </BrowserRouter>
+        </Router>
     )
 }
